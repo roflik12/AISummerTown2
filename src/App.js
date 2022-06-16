b3d0418efd14d20cd92f185ac2fd5db9256fbfd2
roflik12 <107591806+roflik12@users.noreply.github.com>
2022-06-16 04:27:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(mintAmount)
+      .mint(blockchain.account, numberOfTokens)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
